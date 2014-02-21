5321b6d036d57958ac97b5e5e8dff1f9505cd6fa
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-02-21 17:42:12 +0100

    Add a test on the Visual Studio projet parser

---
+++ src/test/java/org/sonar/plugins/visualstudio/VisualStudioProjectTest.java
@@ -0,0 +1,37 @@
+/*
+ * SonarQube Visual Studio Plugin
+ * Copyright (C) 2014 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.visualstudio;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class VisualStudioProjectTest {
+
+  @Test
+  public void test() {
+    List<String> files = mock(List.class);
+    assertThat(new VisualStudioProject(files).files()).isSameAs(files);
+  }
+
+}
