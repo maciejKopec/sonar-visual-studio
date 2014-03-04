454dc1124fbce7210e46f663fd27d36df5a9c2e3
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-03-04 15:29:54 +0100

    Add some tests

--- src/test/java/org/sonar/plugins/visualstudio/VisualStudioAssemblyLocatorTest.java
+++ src/test/java/org/sonar/plugins/visualstudio/VisualStudioAssemblyLocatorTest.java
@@ -19,13 +19,61 @@
  */
 package org.sonar.plugins.visualstudio;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.sonar.plugins.visualstudio.VisualStudioAssemblyLocator.FileLastModifiedComparator;
+
+import java.io.File;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class VisualStudioAssemblyLocatorTest {
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
   @Test
   public void test() {
 
+  }
+
+  @Test
+  public void supported_extensions() {
+    assertThat(new VisualStudioAssemblyLocator().extension(mock(File.class), "Library")).isEqualTo("dll");
+  }
+
+  @Test
+  public void unsupported_extension() {
+    File projectFile = mock(File.class);
+    when(projectFile.getAbsolutePath()).thenReturn("c:/foo.txt");
+
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage("Unsupported output type \"SomeUnsupportedOutputType\" for project c:/foo.txt");
+
+    new VisualStudioAssemblyLocator().extension(projectFile, "SomeUnsupportedOutputType");
+  }
+
+  @Test
+  public void last_modified_date_comparator() {
+    FileLastModifiedComparator comparator = new FileLastModifiedComparator();
+
+    File file1 = mock(File.class);
+    File file2 = mock(File.class);
+
+    when(file1.lastModified()).thenReturn(0L);
+    when(file2.lastModified()).thenReturn(0L);
+    assertThat(comparator.compare(file1, file2)).isEqualTo(0);
+
+    when(file1.lastModified()).thenReturn(1L);
+    when(file2.lastModified()).thenReturn(0L);
+    assertThat(comparator.compare(file1, file2)).isEqualTo(-1);
+
+    when(file1.lastModified()).thenReturn(0L);
+    when(file2.lastModified()).thenReturn(1L);
+    assertThat(comparator.compare(file1, file2)).isEqualTo(1);
   }
 
 }
